cea5c9ec6fc475aad10e14deccddc50478eec2af
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-10 12:08:02 +0530

    Fetching & filtering events data with ssr

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,36 +1,20 @@
-'use client';
-
 import EventCard from "@/components/EventCard";
-import { useEffect, useState } from "react";
 
-const EventsPage = ({ searchParams }) => {
+const EventsPage = async ({ searchParams }) => {
     const EVENTS_URL = 'https://qevent-backend.labs.crio.do/events';
-    const [events, setEvents] = useState(null);
-    const [filteredEvents, setFilteredEvents] = useState(null);
 
     const fetchEvents = async () => {
-        const res = await fetch(EVENTS_URL, { next: { revalidate: 600 } });
+        const res = await fetch(EVENTS_URL, {cache: 'no-store'});
 
         if (!res || !res.ok) {
             throw new Error('Failed to fetch data')
         }
 
-        return res.json();
+        return await res.json();
     }
 
-    useEffect(() => {
-        (async () => {
-            const data = await fetchEvents();
-            setEvents(data);
-        })();
-    }, [searchParams]);
-
-    useEffect(() => {
-        if (events && events.length > 0) {
-            const filteredData = searchParams.artist ? events.filter((event) => event.artist === searchParams.artist) : events;
-            setFilteredEvents(filteredData);
-        }
-    }, [events]);
+    const events = await fetchEvents();
+    const filteredEvents = searchParams && searchParams.artist ? events.filter((event) => event.artist === searchParams.artist) : events;
 
     return (
         <div className="flex flex-wrap justify-around">
