352a2dcd4f95de56f85d3211b19466becffd7a36
AshuJojo <ashutoshsharma.120599@gmail.com>
2024-12-10 11:34:16 +0530

    fetching & displaying events in /events url

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,6 +1,26 @@
-const EventsPage = () => {
+import EventCard from "@/components/EventCard";
+
+const EventsPage = async () => {
+    const EVENTS_URL = 'https://qevent-backend.labs.crio.do/events';
+
+    const fetchEvents = async () => {
+        const res = await fetch(EVENTS_URL);
+
+        if (!res.ok) {
+            throw new Error('Failed to fetch data')
+        }
+
+        return res.json();
+    }
+
+    const events = await fetchEvents();
+
     return (
-        <div>Events Page</div>
+        <div className="flex flex-wrap">
+            {events?.map((event) => (
+                <EventCard key={event.id} eventData={event} />
+            ))}
+        </div>
     )
 }
 
